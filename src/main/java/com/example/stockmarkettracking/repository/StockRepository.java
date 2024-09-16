8ae08ba18da3852d35ea7ae68b9ab6d2233dc7d9
ebd <egemenbdurmaz@gmail.com>
2024-09-16 14:40:44 +0200

    repos updated

--- src/main/java/com/example/stockmarkettracking/repository/StockRepository.java
+++ src/main/java/com/example/stockmarkettracking/repository/StockRepository.java
@@ -3,6 +3,6 @@ package com.example.stockmarkettracking.repository;
 import com.example.stockmarkettracking.model.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StockRepository extends JpaRepository<Stock, Integer> {
+public interface StockRepository extends JpaRepository<Stock, Long> {
     Stock findBySymbol(String symbol);
 }
--- src/main/java/com/example/stockmarkettracking/repository/UserRepository.java
+++ src/main/java/com/example/stockmarkettracking/repository/UserRepository.java
@@ -1,9 +1,8 @@
 package com.example.stockmarkettracking.repository;
 
-
 import com.example.stockmarkettracking.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String userName);
+    User findByUsername(String username);
 }
