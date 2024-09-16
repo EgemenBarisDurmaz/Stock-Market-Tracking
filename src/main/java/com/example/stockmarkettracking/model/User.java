a229d66a203a8a9ea8dd246b60800db949aa4118
ebd <egemenbdurmaz@gmail.com>
2024-09-16 14:37:29 +0200

    input validator added

--- src/main/java/com/example/stockmarkettracking/model/User.java
+++ src/main/java/com/example/stockmarkettracking/model/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,7 +17,13 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Username is required")
     private String username;
+
+    @NotBlank(message = "Password is required")
     private String password;
+
+    @NotBlank(message = "Role is required")
     private String roles;
 }
