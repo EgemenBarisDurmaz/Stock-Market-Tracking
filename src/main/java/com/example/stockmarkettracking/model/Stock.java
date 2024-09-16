cc109b81115fa689653664063a602a9ebe14ad91
ebd <egemenbdurmaz@gmail.com>
2024-09-16 14:34:02 +0200

    input validator added

--- src/main/java/com/example/stockmarkettracking/model/Stock.java
+++ src/main/java/com/example/stockmarkettracking/model/Stock.java
@@ -1,10 +1,13 @@
 package com.example.stockmarkettracking.model;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,11 +23,18 @@ public class Stock {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Symbol is required")
+    @Pattern(regexp = "^[A-Z]{1,5}$", message = "Invalid stock symbol")
     private String symbol;
+
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @Positive(message = "Price must be positive")
     private double price;
+
+    @NotBlank(message = "Currency is required")
+    @Pattern(regexp = "^[A-Z]{3}$", message = "Invalid currency code")
     private String currency;
-    private LocalDateTime createdDate;
-    private LocalDateTime lastUpdatedDate;
-    private String internalCode; 
+
 }
