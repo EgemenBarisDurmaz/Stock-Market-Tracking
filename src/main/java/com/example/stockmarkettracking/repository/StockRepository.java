6ea511e82ea4090703f17e307563d500f9435cac
ebd <egemenbdurmaz@gmail.com>
2024-09-17 15:03:19 +0200

    repos updated

--- src/main/java/com/example/stockmarkettracking/repository/StockRepository.java
+++ src/main/java/com/example/stockmarkettracking/repository/StockRepository.java
@@ -3,6 +3,11 @@ package com.example.stockmarkettracking.repository;
 import com.example.stockmarkettracking.model.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StockRepository extends JpaRepository<Stock, Long> {
-    Stock findBySymbol(String symbol);
+
+    Optional<Stock> findBySymbol(String symbol);
+
+    Optional<Stock> findByPublicId(String publicId);
 }
