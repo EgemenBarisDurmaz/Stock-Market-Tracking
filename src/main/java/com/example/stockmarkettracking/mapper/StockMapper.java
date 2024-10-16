674cfddd5b74b39414b994e18032535d6beb7a54
EgemenBarisDurmaz <egemenbdurmaz@gmail.com>
2024-10-16 20:05:49 +0200

    update Stock Mapper

--- src/main/java/com/example/stockmarkettracking/mapper/StockMapper.java
+++ src/main/java/com/example/stockmarkettracking/mapper/StockMapper.java
@@ -1,13 +1,19 @@
 package com.example.stockmarkettracking.mapper;
 
 import com.example.stockmarkettracking.dto.StockDTO;
+import com.example.stockmarkettracking.dto.input.StockCreationDTO;
+import com.example.stockmarkettracking.dto.input.StockUpdateDTO;
+import com.example.stockmarkettracking.dto.output.StockRetrievalDTO;
 import com.example.stockmarkettracking.model.Stock;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface StockMapper {
 
-    StockDTO toStockDTO(Stock stock);
+    StockRetrievalDTO toStockRetrievalDTO(Stock stock);
 
-    Stock toStock(StockDTO stockDTO);
+    Stock toStock(StockCreationDTO stockCreationDTO );
+
+    void updateStockFromDto(StockUpdateDTO stockUpdateDTO, @MappingTarget Stock stock);
 }
