7e79a53380a2c4d460aa72f5bcc1840064ff9d9a
EgemenBarisDurmaz <egemenbdurmaz@gmail.com>
2024-10-16 20:05:57 +0200

    update Stock Entity

--- src/main/java/com/example/stockmarkettracking/model/Stock.java
+++ src/main/java/com/example/stockmarkettracking/model/Stock.java
@@ -2,7 +2,6 @@ package com.example.stockmarkettracking.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
@@ -22,16 +21,12 @@ public class Stock {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "Symbol is required")
-    @Pattern(regexp = "^[A-Z]{1,5}$", message = "Invalid stock symbol")
     @Column(nullable = false, unique = true)
     private String symbol;
 
-    @NotBlank(message = "Name is required")
     @Column(nullable = false)
     private String name;
 
-    @Positive(message = "Price must be positive")
     @Column(nullable = false)
     private double price;
 
