232daf81d3fee41b364c7804217f967552504550
EgemenBarisDurmaz <egemenbdurmaz@gmail.com>
2024-10-16 22:06:19 +0200

    Update Repos

--- src/main/java/com/example/stockmarkettracking/repository/StockRepository.java
+++ src/main/java/com/example/stockmarkettracking/repository/StockRepository.java
@@ -2,12 +2,11 @@ package com.example.stockmarkettracking.repository;
 
 import com.example.stockmarkettracking.model.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.Nullable;
 
 import java.util.Optional;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
-
-    Optional<Stock> findBySymbol(String symbol);
 
     Optional<Stock> findById(Long id);
 }
