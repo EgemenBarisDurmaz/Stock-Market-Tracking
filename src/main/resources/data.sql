3a34d6d539f302247812ece16d59b99090fd6ea0
EgemenBarisDurmaz <egemenbdurmaz@gmail.com>
2024-10-18 18:16:01 +0200

    create data.sql with first entries

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into users (username, password) VALUES ('egemendrmz', 'ege123');
+insert into users (username, password) VALUES ('egebaris', 'ege125');
+
+insert into stock (symbol, name, price) VALUES ('AAPL', 'apple', 100.00);
+insert into stock (symbol, name, price) VALUES ('GOOGL', 'google', 10.00);
